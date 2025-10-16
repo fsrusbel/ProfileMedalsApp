d557ed25d432b210a7eba7986efb35809092700d
Rusbel Jayo Flores Santillan <ntt-rusbel.flores@mibanco.com.pe>
2025-10-16 10:02:50 -0500

    Initial Commit

---
+++ ProfileMedalsApp/ProfileMedalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileMedalsAppApp.swift
+//  ProfileMedalsApp
+//
+//  Created by usuariolocal on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileMedalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
